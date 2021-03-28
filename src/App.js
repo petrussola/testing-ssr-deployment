63f56bd456059568813b2039a08e0227afacc3f7
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-28 21:17:53 +0100

    SSR

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
 
 const App = () => {
   return <div>Whatever</div>;
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
