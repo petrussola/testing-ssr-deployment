6f70a499a7662bc58540f93788c153224a3ad515
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-28 17:46:52 +0100

    MOve browser component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  return <div>Whatever</div>;
+  return (
+    <Router>
+      <div>Whatever</div>;
+    </Router>
+  );
 };
 
 export default App;
